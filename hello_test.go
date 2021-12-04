50bc263433e7b568c450100291edf5c53d61fea3
opti21 <40129778+opti21@users.noreply.github.com>
2021-12-03 23:10:36 -0600

    added default string for empty string and tests

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Brandon")
-	want := "Hello, Brandon"
+	assertCorrectMessage := func (t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Brandon")
+		want := "Hello, Brandon"
+
+		assertCorrectMessage(t, got, want)
+	}) 
+
+	t.Run("say 'Hello, world' when an empty string", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+		
+	})
 }
