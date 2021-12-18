80b96d42dfbcefafb35a9dd1335428e4bd4091ff
opti21 <40129778+opti21@users.noreply.github.com>
2021-12-18 12:09:35 -0600

    2nd init

---
+++ structs/structs.go
@@ -0,0 +1,28 @@
+package structs
+
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func Perimeter(reactangle Rectangle) float64 {
+	return 2 * (reactangle.Width + reactangle.Height)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
